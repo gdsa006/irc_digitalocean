1a14f832c68ec8f635cdb0d08a1defdf6a1c83c9
gdsa006 <gdsa006@gmail.com>
2022-09-15 16:21:45 +0530

    fix

--- app/Http/Controllers/LeadsController.php
+++ app/Http/Controllers/LeadsController.php
@@ -544,7 +544,7 @@ class LeadsController extends Controller
                 $guzzleResponse = $client->post(
                         'https://rest.gohighlevel.com/v1/contacts/', [
                         'headers' => [
-                            'APP_KEY'=>'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2NhdGlvbl9pZCI6IkQ4WVRWb2hnSnJaYU5iZHI0c25kIiwiY29tcGFueV9pZCI6InFwckhpT0JUTVRkVDBhZGpXTEtqIiwidmVyc2lvbiI6MSwiaWF0IjoxNjYwODM1NTM2ODcwLCJzdWIiOiJ2cDlKaEpwbmxkYzhuUklPYXR1UCJ9.6gamZ1OOjc9DfH03Ro3n4M0c1YHeZ6vRrrJwQ2NbR5k'
+                            'APP_KEY'=>'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2NhdGlvbl9pZCI6IkQ4WVRWb2hnSnJaYU5iZHI0c25kIiwiY29tcGFueV9pZCI6InFwckhpT0JUTVRkVDBhZGpXTEtqIiwidmVyc2lvbiI6MSwiaWF0IjoxNjYwODM1NTM2ODcwLCJzdWIiOiJ2cDlKaEpwbmxkYzhuUklPYXR1UCJ9.6gamZ1OOjc9DfH03Ro3n4M0c1YHeZ6vRrrJwQ2NbR5k'
                         ],
                         'form_params' => [
                             'firstname' => 'Dan',
